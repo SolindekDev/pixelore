48e5af84391993aa25233ba9928693358d8156d7
SolindekDev <solindeklive.biznes@gmail.com>
2023-02-04 20:03:59 +0100

    Better build.sh i think so lol

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ mkdir -p build
 for c_file in $C_SOURCES
 do
     echo gcc -c -O3 -std=c18 -Wall -I/usr/include/SDL2 -Iinclude $c_file -o ./build/$c_file.o
-    gcc -c -O3 -std=c18 -Wall -Wno-unused-variable -I/usr/include/SDL2 -Iinclude $c_file -o ./build/$c_file.o
+    gcc -c -O3 -std=c18 -Wall -Wno-unused-variable $(sdl2-config --cflags) -Iinclude $c_file -o ./build/$c_file.o
 done
 
 OBJECTS=`find ./build -name "*.c.o"`
 echo gcc $OBJECTS -lSDL2 -lSDL2_ttf -o ./pixelore
-gcc $OBJECTS -lSDL2 -lSDL2_ttf -lSDL2_image -o ./pixelore
+gcc $OBJECTS $(sdl2-config --libs) -lSDL2_ttf -lSDL2_image -o ./pixelore
